203e4e74b5098ece9739e47d1f5b6056054ee1b4
Sophie Johnson <91133514+philosophiekatt@users.noreply.github.com>
2023-10-22 16:10:33 +0200

    Add files via upload

---
+++ curves_runs/run_curves.sh
@@ -0,0 +1,8 @@
+/scratch-store/store/skj_software_tools/curves/curves+/Cur+ <<!
+ &inp file=bna, lis=r+bdna,
+ lib=/scratch-store/store/skj_software_tools/curves/curves+/standard,
+ &end
+2 1 -1 0 0 
+1:146
+292:147 
+!
